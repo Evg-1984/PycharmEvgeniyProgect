200a67ce4add30e7788eaee137797eb7d212871f
kojuhove <evgeniy120809@gmail.com>
2025-01-18 19:07:49 +0700

    ver 0.2.1

--- main.py
+++ main.py
@@ -56,39 +56,110 @@ class Button(pygame.sprite.Sprite):
     def get_status(self):
         return self.status
 
-    def render(self, ekran):
+    def update(self, ekran):
+        mouse_pos = pygame.mouse.get_pos()
+        if (self.left <= mouse_pos[0] <= self.left + self.width
+                and self.top <= mouse_pos[1] <= self.top + self.height):
+            pygame.draw.rect(ekran, self.border_color, (self.left - self.border_size,
+                                                         self.top - self.border_size,
+                                                         self.width + self.border_size * 2,
+                                                         self.height + self.border_size * 2), self.border_size)
+            pygame.draw.rect(ekran, self.color, (self.left,
+                                                    self.top,
+                                                    self.width,
+                                                    self.height), 0)
+        else:
             pygame.draw.rect(ekran, self.border_color, (self.left,
-                                                         self.top,
-                                                         self.width,
-                                                         self.height), self.border_size)
+                                                        self.top,
+                                                        self.width,
+                                                        self.height), self.border_size)
             pygame.draw.rect(ekran, self.color, (self.left + self.border_size,
-                                                    self.top + self.border_size,
-                                                    self.width - self.border_size * 2,
-                                                    self.height - self.border_size * 2), 0)
-            font = pygame.font.Font(None, self.text_size)
-            text = font.render(self.text, True, (100, 255, 100))
-            text_x = (self.left + self.width) // 2 - text.get_width() // 2
-            text_y = (self.top + self.height) // 2 - text.get_height() // 2
-            ekran.blit(text, (text_x, text_y))
-
+                                                 self.top + self.border_size,
+                                                 self.width - self.border_size * 2,
+                                                 self.height - self.border_size * 2), 0)
+        font = pygame.font.Font(None, self.text_size)
+        text = font.render(self.text, True, (100, 255, 100))
+        text_x = (self.left + self.width // 2) - text.get_width() // 2
+        text_y = (self.top + self.height // 2) - text.get_height() // 2
+        ekran.blit(text, (text_x, text_y))
+        self.mouse_on_button = False
     def on_click(self):
             if self.status == True:
                 self.status = False
             else:
                 self.status = True
 
-    def get_click(self, mouse_pos):
+    def get_click(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
             if (self.left <= mouse_pos[0] <= self.left + self.width
-                    and self.top <= mouse_pos[1] <= self.top + self.height):
+                and self.top <= mouse_pos[1] <= self.top + self.height):
                 self.on_click()
 
 
-all_sprites = pygame.sprite.Group()
 
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+all_sprites = pygame.sprite.Group()
+buttons = pygame.sprite.Group()
 fps = 60
 clock = pygame.time.Clock()
 running = True
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    start_bt = Button(buttons, status=False, text="начать", text_size=40,
+                      width=280, height=60, coords=(width // 2 - 140, height // 4 - 30), color=(255, 255, 255),
+                      border_color=(0, 0, 0), border_size=5)
+    end_bt = Button(buttons, status=False, text="выйти", text_size=40,
+                      width=280, height=60, coords=(width // 2 - 140, height - height // 4 - 30), color=(255, 255, 255),
+                      border_color=(0, 0, 0), border_size=5)
+
+    while True:
+        screen.fill((255, 255, 255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            start_bt.get_click(event)
+            end_bt.get_click(event)
+
+        buttons.update(screen)
+        if start_bt.get_status():
+            return
+        if end_bt.get_status():
+            terminate()
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+start_screen()
 while running:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
